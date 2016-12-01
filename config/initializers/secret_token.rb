def1426d943019aa456e91612aaf47e88e773506
Aleks Real <aleksanderreal@gmail.com>
2016-12-01 06:37:05 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuickFlash::Application.config.secret_key_base = '1f64f3667ca46ff993e7e8ad9839f5726ccc99fcc0b599476b4fe14082540fcb3efc0b48def2006230b97bcea53f515c1c417240e90764078a97bab81e1c72ea'
